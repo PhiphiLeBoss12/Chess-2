24e380be1a11af6283dff3c698da0a32d207db1e
Lilian <liliannalil347@gmail.com>
2023-06-07 10:14:59 +0200

    Update Main.c

--- Chess/src/Main.c
+++ Chess/src/Main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 int main(int argc, char* argv[])
 {
     printf("Hello world!\n");
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("a", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, NULL, NULL);
+    SDL_Window* window = SDL_CreateWindow("Chess 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
