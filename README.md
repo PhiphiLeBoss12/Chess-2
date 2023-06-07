7d4e0ab23594008d5d9bff82813a70dd658be436
TotoooDev <totooodev@gmail.com>
2023-06-07 15:57:22 +0200

    Added cool tutorial

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Chess 2
 ## The long awaited sequel
+
+## Comment ça marche premake ?
+
+[Docs de premake](https://premake.github.io/docs/)
+
+Premake est un petit programme tout sympa qui permet de générer des projets pour plein d'IDE différents. En gros ça permet de pouvoir faciliter le partage du projet sur plusieurs plateformes différentes.
+
+Il utilise le fichier `premake5.lua` pour connaître la spécification du projet. J'ai mis plein de jolis commentaires le fichier pour pas être perdu si vous devez vous y aventurer.
+
+### Ajouter une bibliothèque
+
+Pour ajouter une bibliothèque il faut :
+- Ajouter les fichiers `*.lib` dans le dossier `libs/windows/` du projet.
+- Aller dans `premake5.lua`, dans `filter 'system:windows'` trouver la partie `links`
+- Ajouter le nom de la bibliothèque sans l'extension
+- ???
+- Profit
--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
--- Ripped from Cherno's video on premake
+-- Stolen from Cherno's video on premake
 -- https://www.youtube.com/watch?v=sULV3aB2qeU
 
+-- Defines the name of the workspace (the Visual Studio .sln file)
 workspace "Chess2"
 	architecture "x64"
 
@@ -14,15 +15,18 @@ workspace "Chess2"
 
 outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Defines the project (the .vcxproj file in Chess/)
 project "Chess"
 	location "Chess"
 	kind "ConsoleApp"
 	language "C"
 	cdialect "C17"
 	
+	-- Defines the directory of the executable and the object files
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-intermediate/" .. outputDir .. "/%{prj.name}")
 
+	-- Specifies the files to include in the project
 	files
 	{
 		"%{prj.name}/src/**.h",
@@ -30,6 +34,7 @@ project "Chess"
 		"%{prj.name}/src/vendor/**.c",
 	}
 
+	-- Specifies the include directories
 	includedirs
 	{
 		"Chess/src",
@@ -37,10 +42,12 @@ project "Chess"
 		"Chess/src/vendor"
 	}
 
+	-- If the project is generated on Windows it will apply these rules
 	filter "system:windows"
 		staticruntime "On"
 		systemversion "latest"
 		libdirs ("libs/windows")
+		-- Libraries to link
 		links
 		{
 			"SDL2",
@@ -48,6 +55,7 @@ project "Chess"
 		}
 		postbuildcommands ("xcopy ..\\libs\\windows\\dlls\\ ..\\bin\\" .. outputDir .. "\\%{prj.name}\\ /s /e /y /i")
 
+	-- If the project is generated on Linux it will apply these rules
 	filter "system:linux"
 		staticruntime "On"
 		systemversion "latest"
@@ -58,19 +66,15 @@ project "Chess"
 			"SDL2main"
 		}
 
+	-- If the project is generated with the Debug configuration it will apply these rules
 	filter "configurations:Debug"
 		defines
 		{
 			"CHESS_DEBUG",
-			"CHESS_DO_ASSERT",
-			"CHESS_OPENGL_DEBUG"
+			"CHESS_DO_ASSERT"
 		}
 		symbols "On"
 
+	-- If the project is generated with the Debug configuration it will apply these rules
 	filter "configurations:Release"
-		defines
-		{
-			"CHESS_DEBUG",
-			"CHESS_DO_ASSERT",
-		}
 		optimize "On"
