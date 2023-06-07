190ab8422195915cd6c0f061c8aa16ac12de5b84
Lilian <liliannalil347@gmail.com>
2023-06-07 16:14:27 +0200

    Update Type.h

--- Chess/src/Type.h
+++ Chess/src/Type.h
@@ -5,19 +5,19 @@
 
 typedef enum EnumPiece{PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING} TypePiece;
 typedef enum EnumColor {WHITE, BLACK} TypeColor;
-typedef enum EnumState {COOL, CHECK, CHECKMATE} TypeState; //statut de la partie
+typedef enum EnumState {COOL, CHECK, CHECKMATE} TypeState; //game status
 
 typedef struct StructPiece {
 	TypePiece type;
 	TypeColor color;
 	int x;
 	int y;
-	int hasMoved; //A bougé
+	int hasMoved; //if the piece has moved
 } Piece;
 
 typedef struct StructBoard {
 	int size;
-	Piece table[8][8]; //Tableau de 64
+	Piece table[8][8]; //table of 64 squares
 } Board;
 
 typedef struct StructPlayer {
