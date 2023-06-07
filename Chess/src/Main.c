5e2c17bfa8ef09e7cfe4e6ec76181153e9f2ce8b
Lilian <liliannalil347@gmail.com>
2023-06-07 11:30:50 +0200

    prout2

--- Chess/src/Main.c
+++ Chess/src/Main.c
@@ -7,12 +7,7 @@ int main(int argc, char* argv[])
 {
     printf("Hello world!\n");
     SDL_Init(SDL_INIT_VIDEO);
-<<<<<<< Updated upstream
-    SDL_Window* window = SDL_CreateWindow("Chess 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-=======
-    SDL_Window* window = SDL_CreateWindow("Chess 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL);
-    
->>>>>>> Stashed changes
+    SDL_Window* window = SDL_CreateWindow("Chess 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);    
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
