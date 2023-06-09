de2cb90e3e47e8f4390d0ba552ccd204a722e22f
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-06-09 09:19:24 +0200

    player : création d'une fonction pour l'échec, pour lilian

---
+++ Chess/src/Player.c
@@ -0,0 +1,8 @@
+#include "Player.h"
+
+/*
+Returns a bool that states if the player is check.
+*/
+int isPlayerCheck(Player* player) {
+	return 1; /* Temporaire */
+}
---
+++ Chess/src/Player.h
@@ -0,0 +1,5 @@
+#include "Types.h"
+
+
+Player* initPlayer(TypeColor color);
+int isPlayerCheck(Player* player);
