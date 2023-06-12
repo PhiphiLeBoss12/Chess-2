ac776b4626845f362d96b42b923fdb82db9ae462
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2023-06-12 11:19:19 +0200

    .

--- Chess/src/Main.c
+++ Chess/src/Main.c
@@ -11,32 +11,32 @@
 int main(int argc, char* argv[])
 {
 
-    Board* board;
-    board = malloc(sizeof(Board));
-    Piece* tab[8][8];
-    board->size = 64;
-    board->table = tab;
-    for (int i = 0; i < 8; i++) {
-        board->table[i] = malloc(sizeof(Piece*)*8);
-        for (int j = 0; j < 8; j++) {
-            board->table[i][j] = NULL;
-        }
-    }
+    //Board* board;
+    //board = malloc(sizeof(Board));
+    //Piece* tab[8][8];
+    //board->size = 64;
+    //board->table = tab;
+    //for (int i = 0; i < 8; i++) {
+    //    board->table[i] = malloc(sizeof(Piece*)*8);
+    //    for (int j = 0; j < 8; j++) {
+    //        board->table[i][j] = NULL;
+    //    }
+    //}
 
-    Piece* piece;
-    piece = initPiece(BISHOP, WHITE, 1, 0, board);
-    showPiece(piece);
+    //Piece* piece;
+    //piece = initPiece(BISHOP, WHITE, 1, 0, board);
+    //showPiece(piece);
 
-    //Piece* pieceB;
-    //pieceB = initPiece(PAWN, WHITE, 0, 1, board);
+    ////Piece* pieceB;
+    ////pieceB = initPiece(PAWN, WHITE, 0, 1, board);
 
-    Case* tabCase;
-    int* lenTab = malloc(sizeof(int));
-    tabCase = movePossibilitiesPawn(piece, board, lenTab);
-    for (int i = 0; i < *lenTab; i++) {
-        showCoord(tabCase[i].x, tabCase[i].y);
-        printf("\n");
-    }
+    //Case* tabCase;
+    //int* lenTab = malloc(sizeof(int));
+    //tabCase = movePossibilitiesPawn(piece, board, lenTab);
+    //for (int i = 0; i < *lenTab; i++) {
+    //    showCoord(tabCase[i].x, tabCase[i].y);
+    //    printf("\n");
+    //}
     game();
     return 0;
 }
