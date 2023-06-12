5a18b5655cd3594887298bb8c860621b04e6cf23
TotoooDev <totooodev@gmail.com>
2023-06-12 11:42:17 +0200

    added credits

--- Chess/src/UI.c
+++ Chess/src/UI.c
@@ -8,23 +8,35 @@ void drawStartScreen(Window* window, SDL_Texture** textures) {
 	setDrawColor(window, 64, 64, 64, 255);
 	drawRect(window, &rect);
 
-	SDL_Color color = { 220, 220, 220, 255 };
-	drawText(window, color, "chess 2", 20, 70, 1.0f);
-	drawText(window, color, "the long awaited sequel", 200, sin(iter / 100) * 10 + 55, 0.2f);
-
-	drawText(window, color, "press return to play", 30, 700, 0.3f);
-	drawText(window, color, "press escape to quit :(", 30, 650, 0.3f);
-
 	Rect texRect = { 800, 300, 256, 256, -iter / 5 };
 	drawTexture(window, &texRect, textures[4]);
 	Rect texRect1 = { 600, 600, 128, 128, iter };
 	drawTexture(window, &texRect1, textures[9]);
 	Rect texRect2 = { 100, 300, 128, 128, iter / 6 };
 	drawTexture(window, &texRect2, textures[5]);
-	Rect texRect3 = { cos(iter / 50) * 40 + 500, sin(iter / 50) * 40 + 176, 64, 64, -iter};
+	Rect texRect3 = { cos(iter / 50) * 40 + 500, sin(iter / 50) * 40 + 176, 64, 64, iter / 2 };
 	drawTexture(window, &texRect3, textures[10]);
-	Rect texRect4 = { sin(iter / 70) * 50 + 900, cos(iter / 70) * 50 + 100, 128, 128, iter};
+	Rect texRect4 = { sin(iter / 70) * 50 + 900, cos(iter / 70) * 50 + 100, 128, 128, iter };
 	drawTexture(window, &texRect4, textures[1]);
+
+	SDL_Color color = { 220, 220, 220, 255 };
+	drawText(window, color, "chess 2", 20, 70, 1.0f);
+	drawText(window, color, "the long awaited sequel", 200, sin(iter / 100) * 10 + 55, 0.2f);
+
+	drawText(window, color, "press return to play", 30, 700, 0.3f);
+	drawText(window, color, "press escape to quit :(", 30, 650, 0.3f);
+	drawText(window, color, "press c to see the credits", 30, 600, 0.3f);
+
+	if (window->keyDown == SDLK_c) {
+		drawText(window, color, "supervisor: éric andrès", 450, 550, 0.2f);
+		drawText(window, color, "development: julien", 450, 525, 0.2f);
+		drawText(window, color, "development: lilian", 450, 500, 0.2f);
+		drawText(window, color, "development: marie", 450, 475, 0.2f);
+		drawText(window, color, "development: philéas", 450, 450, 0.2f);
+		drawText(window, color, "development: tom", 450, 425, 0.2f);
+		drawText(window, color, "graphics design: wikipedia", 450, 400, 0.2f);
+		drawText(window, color, "original idea: Hán Xin", 450, 375, 0.2f);
+	}
 
 	iter++;
 }
