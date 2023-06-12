840d381255554ddc593189c5163852d3fa648538
Lilian <liliannalil347@gmail.com>
2023-06-12 11:29:28 +0200

    Update Bots.h

--- Chess/src/Bots.h
+++ Chess/src/Bots.h
@@ -3,3 +3,5 @@
 #include "Player.h"
 #include "Piece.h"
 #include "Window.h"
+
+void RandomBot(Window* window, Board* board, Piece** selectedPiece, Player** players, Cell* possibilities, int numPossibilities, int squareSize, TypeColor* whoPlays);
